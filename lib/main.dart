18f1a6cd9df9d3a9cfe4cc20252a439ee5d756e8
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-09-09 08:54:52 +0900

    loading page

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final stores = <Store>[];
+  var isLoading = true;
+
   Future<void> fetch() async {
+    setState(() {
+      isLoading = true;
+    });
     var url = Uri.parse('https://gist.githubusercontent.com/'
         'junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/'
         '063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
@@ -35,24 +40,48 @@ class _MyHomePageState extends State<MyHomePage> {
     final jsonResult = jsonDecode(response.body);
     final jsonStores = jsonResult['stores'];
 
-    stores.clear();
-    jsonStores.forEach((e) {
-      stores.add(Store.fromJson(e));
+    setState(() {
+      stores.clear();
+      jsonStores.forEach((e) {
+        stores.add(Store.fromJson(e));
+      });
+      isLoading = false;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('마스크 재고 있는 곳 : 0곳')),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('press me'),
-          onPressed: () async {
-            await fetch();
-            print(stores.toString());
-          },
+        appBar: AppBar(
+          title: Text('마스크 재고 있는 곳 : ${stores.length}곳'),
+          actions: [IconButton(icon: Icon(Icons.refresh), onPressed: fetch)],
         ),
+        body: isLoading
+            ? loadingWidget()
+            : ListView(
+                children: stores.map((e) {
+                return ListTile(
+                  title: Text(e.name!),
+                  subtitle: Text(e.addr!),
+                  trailing: Text(e.remainStat ?? '매진'),
+                );
+              }).toList()));
+  }
+
+  Widget loadingWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('정보를 가져오는 중'),
+          CircularProgressIndicator(),
+        ],
       ),
     );
   }
