d91b828cef3708b290851c117c17ebef00e6b849
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-09-09 13:33:04 +0900

    Complete without architect

--- lib/main.dart
+++ lib/main.dart
@@ -59,17 +59,25 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('마스크 재고 있는 곳 : ${stores.length}곳'),
+          title: Text('마스크 재고 있는 곳 : ${stores.where((e) {
+            return e.remainStat == 'plenty' ||
+                e.remainStat == 'some' ||
+                e.remainStat == 'few';
+          }).length}곳'),
           actions: [IconButton(icon: Icon(Icons.refresh), onPressed: fetch)],
         ),
         body: isLoading
             ? loadingWidget()
             : ListView(
-                children: stores.map((e) {
+                children: stores.where((e) {
+                return e.remainStat == 'plenty' ||
+                    e.remainStat == 'some' ||
+                    e.remainStat == 'few';
+              }).map((e) {
                 return ListTile(
                   title: Text(e.name!),
                   subtitle: Text(e.addr!),
-                  trailing: Text(e.remainStat ?? '매진'),
+                  trailing: _buildRemainStatWidget(e),
                 );
               }).toList()));
   }
@@ -83,6 +91,45 @@ class _MyHomePageState extends State<MyHomePage> {
           CircularProgressIndicator(),
         ],
       ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = 'break';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+    }
+    return Column(
+      children: [
+        Text(remainStat,
+            style: TextStyle(color: color, fontWeight: FontWeight.bold)),
+        Text(description)
+      ],
     );
   }
 }
